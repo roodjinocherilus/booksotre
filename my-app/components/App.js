55c3ae3508543172016ee98663ed9b3df2f1c59a
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-05-14 01:22:42 -0400

    create components

---
+++ my-app/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import CategoriesPage from './components/CategoriesPage';
+
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
